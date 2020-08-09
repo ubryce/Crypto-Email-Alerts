267fa8492962361ed66e3dfde623e3c46aec5b11
ubryce <bknguyen@ualberta.ca>
2020-08-08 23:35:32 -0600

    packages and setting up app listen

--- server.js
+++ server.js
@@ -3,3 +3,10 @@ const morgan = require('morgan');
 const bodayParser = require('body-parser');
 
 const app = express()
+
+
+const PORT = process.env.PORT
+
+app.listem(PORT, ()=>{
+    console.log(`App listening on port ${PORT}`)
+});
